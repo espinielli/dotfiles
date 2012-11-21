f5e8e5f700725f2a38518880bf656a315e8d1cc3
Enrico Spinielli <enrico.spinielli@gmail.com>
2012-11-21 09:51:09 +0100

    new status ffile

---
+++ .emacs.d/el-get/.status.el
@@ -0,0 +1,23 @@
+((el-get status "required" recipe nil)
+ (psvn status "installed" recipe
+       (:name psvn :description "Subversion interface for emacs" :type http :url "http://www.xsteve.at/prg/emacs/psvn.el"))
+ (yasnippet status "installed" recipe
+	    (:name yasnippet :website "https://github.com/capitaomorte/yasnippet.git" :description "YASnippet is a template system for Emacs." :type github :pkgname "capitaomorte/yasnippet" :features "yasnippet" :pre-init
+		   (unless
+		       (or
+			(boundp 'yas/snippet-dirs)
+			(get 'yas/snippet-dirs 'customized-value))
+		     (setq yas/snippet-dirs
+			   (list
+			    (concat el-get-dir
+				    (file-name-as-directory "yasnippet")
+				    "snippets"))))
+		   :post-init
+		   (put 'yas/snippet-dirs 'standard-value
+			(list
+			 (list 'quote
+			       (list
+				(concat el-get-dir
+					(file-name-as-directory "yasnippet")
+					"snippets")))))
+		   :compile nil :submodule nil)))
